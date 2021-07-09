5c7c3dadda9e2774c4881ff1ff7e2a2eb28d7028
boris <borisv.ca@gmail.com>
2021-07-09 14:25:17 -0700

    Added endpoint functionality

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,8 @@
 import io.vertx.core.AbstractVerticle
-import io.vertx.*
 import io.vertx.core.Vertx
-import io.vertx.core.http.HttpServerOptions
-import io.vertx.kotlin.core.json.json
-import io.vertx.kotlin.core.json.obj
-import io.vertx.ext.*
 import io.vertx.ext.web.Router
-import io.vertx.kotlin.core.http.httpServerOptionsOf
-import kotlin.coroutines.coroutineContext
+import io.vertx.ext.web.RoutingContext
 
-//import io.ktor.routing.*
 
 fun main() {
     val vertx: Vertx = Vertx.vertx()
@@ -19,33 +12,8 @@ fun main() {
 class MainVerticle() : AbstractVerticle() {
 
     override fun start() {
-        // Create a Router
+        // Create a Router to handle incoming requests
         val router = Router.router(vertx)
-        // Mount the handler for all incoming requests at every path and HTTP method
-        router.route().handler { context ->
-            val response = context.response()
-            response.putHeader("content-type", "text-plain")
-                .setChunked(true)
-                .write("hello\n")
-            response.write("HELLO")
-
-            response.end("ended")
-            // Get the address of the request
-            val address = context.request().connection().remoteAddress().toString()
-            // Get the query parameter "name"
-            val queryParams = context.queryParams()
-            val name = queryParams.get("name") ?: "unknown"
-            // Write a json response
-            context.json(
-                json {
-                    obj(
-                        "name" to name,
-                        "address" to address,
-                        "message" to "Hello $name connected from $address"
-                    )
-                }
-            )
-        }
         // Create the HTTP server
         vertx.createHttpServer()
             // Handle every request using the router
@@ -56,5 +24,25 @@ class MainVerticle() : AbstractVerticle() {
             .onSuccess { server ->
                 println("HTTP server started on port " + server.actualPort())
             }
+
+
+        // Mount the handler for all incoming requests at every path and HTTP method
+        // HANDLER 1
+
+        router.route("/bruh1").handler { context ->
+            val response = context.response()
+            response.putHeader("content-type", "text/html")
+                .setChunked(true)
+                .write("<h1 style=\"color: red\">bruh1</h1>")
+            response.end()
+            // Get the address of the request
+        }
+
+        // HANDLER 2
+        router.route("/bruh2").handler {getData(it)}
+    }
+
+    fun getData(routingContext: RoutingContext): String{
+        return "bruh2"
     }
 }
