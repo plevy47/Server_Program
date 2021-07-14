0a7eee0f4680ca16e0a871a07eee4bd2bae576b4
plevy <plevy2001@gmail.com>
2021-07-14 13:07:10 -0400

    fixed everything maybe

--- src/main/kotlin/MainVerticle.kt
+++ src/main/kotlin/MainVerticle.kt
@@ -24,8 +24,8 @@ class MainVerticle : AbstractVerticle() {
                 println("HTTP server started on port " + server.actualPort())
             }
 
-        router.post("/create").handler { createData(it) }
-        router.get("/read").handler { readData(it) }
+        router.route("/create").handler { createData(it) }
+        router.route("/read").handler { readData(it) }
         router.route("/update").handler { updateData(it) }
         router.route("/delete").handler { deleteData(it) }
         router.route().handler { displayDefault(it) }
@@ -45,28 +45,29 @@ class MainVerticle : AbstractVerticle() {
     private fun readData(routingContext: RoutingContext) {
         val params = routingContext.request().params()
         val t = getParams(params)
-        if (params == null) { //this needs to be fixed
+        if (t.name == "all") {
             routingContext.response().end(teamList.toString())
         } else {
-            if (isInHockeyTeams(t)) {
-                routingContext.response().end(t.toString())
-            } else {
-                routingContext.response().setStatusCode(404).end("Data Not Found")
+            for (team in teamList) {
+                if (team.name == t.name) {
+                    routingContext.response().end(team.toString())
+                }
             }
+            routingContext.response().setStatusCode(404).end("Data Not Found")
         }
     }
 
     private fun updateData(routingContext: RoutingContext) {
         val params = routingContext.request().params()
-        val oldName = params.get("oldName")
-        val oldCity = params.get("oldCity")
-        val oldCupWins = params.get("oldCupWins").toInt()
-        val oldIsOriginalSix = params.get("oldIsOriginalSix").toBoolean()
-        val oldTeamColours = params.get("oldTeamColours").split(",")
-        val o = HockeyTeam(oldName, oldCity, oldCupWins, oldIsOriginalSix, oldTeamColours)
-        val n = getParams(params)
+        val newName = params.get("newName")
+        val newCity = params.get("newCity")
+        val newCupWins = params.get("newCupWins").toInt()
+        val newIsOriginalSix = params.get("newIsOriginalSix").toBoolean()
+        val newTeamColours = params.get("newTeamColours").split(",")
+        val n = HockeyTeam(newName, newCity, newCupWins, newIsOriginalSix, newTeamColours)
+        val o = getParams(params)
         if (isInHockeyTeams(o)) {
-            val foundTeam: HockeyTeam? = teamList.find { it.name == oldName }
+            val foundTeam: HockeyTeam? = teamList.find { it.name == newName }
             teamList.remove(foundTeam)
             teamList.add(n)
             routingContext.response().end(teamList.toString())
@@ -77,37 +78,55 @@ class MainVerticle : AbstractVerticle() {
 
     private fun deleteData(routingContext: RoutingContext) {
         val params = routingContext.request().params()
-        if (params == null) {
+        val t = getParams(params)
+        if (t.name == "all") {
             teamList.clear()
             routingContext.response().end("All data cleared")
         } else {
-            val t = getParams(params)
-            if (isInHockeyTeams(t)) {
-                val foundTeam: HockeyTeam? = teamList.find { it.name == t.name && it.homeCity == t.homeCity }
-                teamList.remove(foundTeam)
-                routingContext.response().end("Deleted: $t")
-            } else {
-                routingContext.response().setStatusCode(404).end("Data Not Found")
+            for (team in teamList) {
+                if (team.name == t.name) {
+                    val foundTeam: HockeyTeam? = teamList.find { it.name == t.name && it.homeCity == t.homeCity }
+                    teamList.remove(foundTeam)
+                    routingContext.response().end("Deleted: $t")
+                }
             }
+            routingContext.response().setStatusCode(404).end("Data Not Found")
         }
+
     }
 
     private fun isInHockeyTeams(requestedTeam: HockeyTeam): Boolean {
         for (team in teamList)
-            if (team.name == requestedTeam.name && team.homeCity == requestedTeam.homeCity) {
+            if (team.name == requestedTeam.name) {
                 return true
             }
         return false
     }
 
-    private fun getParams(params: MultiMap): HockeyTeam {
-        val name = params.get("name")
-        val city = params.get("city")
-        val cupWins = params.get("cupWins").toInt()
-        val isOriginalSix = params.get("isOriginalSix").toBoolean()
-        val teamColours = params.get("teamColours").split(",")
 
-        return HockeyTeam(name, city, cupWins, isOriginalSix, teamColours)
+    private fun getParams(params: MultiMap): HockeyTeam {
+        var name = params.get("name")
+        var city = params.get("city")
+        var cupWins = params.get("cupWins")
+        var isOriginalSix = params.get("isOriginalSix")
+        var teamColours = params.get("teamColours")
+        if (name == null) {
+            name = "all"
+        }
+        if (city == null) {
+            city = "none"
+        }
+        if (cupWins == null) {
+            cupWins = "0"
+        }
+        if (isOriginalSix == null) {
+            isOriginalSix = "false"
+        }
+        if (teamColours == null) {
+            teamColours = "none"
+        }
+
+        return HockeyTeam(name, city, cupWins.toInt(), isOriginalSix.toBoolean(), teamColours.split(","))
     }
 }
 
